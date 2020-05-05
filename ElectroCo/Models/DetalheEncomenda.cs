e62ac0450d13a87ea6a0a30b67e6c6a816679f7b
Óscar Campos <oc.darkreaper@gmail.com>
2020-05-05 13:18:50 +0100

    Adicionar o modelo DetalheEncomenda

---
+++ ElectroCo/Models/DetalheEncomenda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectroCo.Models
+{
+    public class DetalheEncomenda
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public float PrecoProduto { get; set; }
+
+        [ForeignKey(nameof(Order))]
+        public int EncomendaID { get; set; }
+        public virtual ICollection<Encomenda> Order { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        public int ProdutoID { get; set; }
+        public virtual ICollection<Produtos> Product { get; set; }
+    }
+}
