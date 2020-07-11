1ee33bd47e7aaa6f6049dc7f65695b402c76f3aa
Óscar Campos <oc.darkreaper@gmail.com>
2020-07-11 19:25:21 +0100

    identity/account

--- ElectroCo/Controllers/HomeController.cs
+++ ElectroCo/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ElectroCo.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ElectroCo.Controllers
 {
@@ -18,12 +19,8 @@ namespace ElectroCo.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
